ce2bae04e09b382044916ba5f940c80fd56f6102
maksim <durov1108@gmail.com>
2018-08-01 12:26:43 +0000

    -

--- strReverse.java
+++ strReverse.java
@@ -3,22 +3,28 @@ package edu;
 public class strReverse {
     public static void main(String... args) throws Exception {
         
-        // String reverse
-        String str = "maksimus";
+        // String reverse v1
+        String str = "Mercury";
         char[] ares = new char[str.length()];
         for (int i = str.length()-1; i >= 0; i--) {
             ares[str.length()-(i+1)] = str.charAt(i);
         }
         System.out.println(str.copyValueOf(ares));
-        
-                
-        // String reverse
-        String str2 = "Superbien";
+
+
+        // String reverse v2
+        String str2 = "Venus";
         for(int y=0; y < str2.length(); y++){
-            System.out.print(str2.charAt((str2.length()-y-1)) + " ");
+            System.out.print(str2.charAt((str2.length()-y-1)) + "");
         }
-        
-        
+        System.out.println();
+        System.out.println(Rev("Mars"));
+
+    }
+
+    // method recursion
+    public static String Rev(String s){
+        return (s.length()>1) ? s.charAt(s.length()-1) + Rev(s.substring(0,s.length()-1)): s ;
     }
 }
 
